785a2f349229b051d8f5e3b9e4fabe4d163e70c8
심승용 <seungyong.shim@mirero.co.kr>
2019-01-31 14:26:59 +0900

    - test code

---
+++ Jaeger.Tutorial.ConsoleApp.AsyncTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Jaeger.Samplers;
+using Microsoft.Extensions.Logging;
+
+namespace Jaeger.Tutorial.ConsoleApp.AsyncTask
+{
+    internal class Program
+    {
+        private static Tracer InitTracer(string serviceName, ILoggerFactory loggerFactory)
+        {
+            Configuration.SenderConfiguration sender = new Configuration.SenderConfiguration(loggerFactory)
+               .WithAgentHost("127.0.0.1")
+               .WithAgentPort(6831);
+
+            Configuration.SamplerConfiguration samplerConfiguration = new Configuration.SamplerConfiguration(loggerFactory)
+                .WithType(ConstSampler.Type)
+                .WithParam(1);
+
+            Configuration.ReporterConfiguration reporterConfiguration = new Configuration.ReporterConfiguration(loggerFactory)
+                .WithSender(sender)
+                .WithLogSpans(true);
+
+            return (Tracer)new Configuration(serviceName, loggerFactory)
+                .WithSampler(samplerConfiguration)
+                .WithReporter(reporterConfiguration)
+                .GetTracer();
+        }
+
+        private static async Task Main(string[] args)
+        {
+            ILoggerFactory loggerFactory = new LoggerFactory();
+            Tracer tracer = InitTracer("Jaeger.Tutorial.ConsoleApp", loggerFactory);
+
+            var pinger = new Teller("Hello", tracer, loggerFactory);
+            var ponger = new Teller("World", tracer, loggerFactory);
+
+            await pinger.IceBreakAsync(ponger); // 먼저 말을 걸기
+
+            Console.In.ReadLine();
+        }
+    }
+}
---
+++ Jaeger.Tutorial.ConsoleApp.AsyncTask/Teller.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using OpenTracing;
+using OpenTracing.Tag;
+
+namespace Jaeger.Tutorial.ConsoleApp.AsyncTask
+{
+    public class Teller
+    {
+        private readonly string _message;
+        private readonly ITracer _tracer;
+        private readonly ILogger<Teller> _logger;
+
+        public async Task TellAsync(string message, Teller teller)
+        {
+            await Task.Factory.StartNew(async () =>
+            {
+                using (IScope scope = _tracer.BuildSpan("TellAsync").StartActive(true))
+                {
+                    try
+                    {
+                        // avoid burst calling func
+                        await Task.Delay(10);
+
+                        scope.Span.Log($"{message}");
+                        Console.Write($"{message} ");
+
+                        await teller.TellAsync(_message, this);
+                    }
+                    catch (Exception ex)
+                    {
+                        Tags.Error.Set(scope.Span, true);
+                    }
+                }
+            });
+        }
+
+        public Teller(string message, ITracer tracer, ILoggerFactory loggerFactory)
+        {
+            _message = message;
+            _tracer = tracer;
+            _logger = loggerFactory.CreateLogger<Teller>();
+        }
+
+        public async Task IceBreakAsync(Teller receiver)
+        {
+            using (IScope scope = _tracer.BuildSpan(nameof(IceBreakAsync)).StartActive(true))
+            {
+                await receiver.TellAsync(_message, this);
+            }
+        }
+    }
+}
